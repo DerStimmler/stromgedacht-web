4d0a7029765a68bc303735214130996fb72ac8a4
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2023-08-13 17:25:34 +0200

    fix: umd import of ts-stromgedacht

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [esbuildCommonjs(['ts-stromgedacht'])]
+		}
+	}
 });
