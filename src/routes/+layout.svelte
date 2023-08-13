463284d4e7824f6afeed2a285e4ce5bea15e3940
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2023-08-13 15:37:56 +0200

    style: add basic configuration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
-    import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<header class="flex flex-col gap-2">
+	<h1 class="text-2xl text-center">StromGedacht Web</h1>
+	<h2 class="text-textMuted text-center">Inoffizielle Webseite, um StromGedacht Daten abzurufen</h2>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<style>
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		colors: {
+			background: '#0C161A',
+			surface: '#182328',
+			text: '#E1E8EA',
+			textMuted: '#626A6E',
+			warning: '#F5D470',
+			warningMuted: '#867C4C',
+			success: '#3DEE8F',
+			successMuted: '#2B885C',
+			danger: '#FA7046',
+			critical: '#AB375C'
+		},
+		fontFamily: {
+			sans: ['Plus Jakarta Sans', 'sans-serif']
+		},
+		extend: {}
+	},
+	plugins: []
+};
