95bc97f1381670382e39e2bc6885f77da0e97836
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2023-08-27 00:48:33 +0200

    feat: add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,18 @@
 
 <header class="flex flex-col gap-2">
 	<h1 class="text-2xl text-center">StromGedacht Web</h1>
-	<h2 class="text-textMuted text-center">Inoffizielle Webseite, um StromGedacht Daten abzurufen</h2>
+	<h2 class="text-textMuted text-center">
+		Inoffizielle Webseite zur Übersicht von StromGedacht Daten
+	</h2>
 </header>
 
-<main>
+<main class="flex-1">
 	<slot />
 </main>
 
-<style>
-</style>
+<footer class="w-full flex justify-between items-center">
+	<a href="https://github.com/derstimmler/stromgedacht-web">
+		<img src="images/github-mark-white.svg" width="32" height="32" alt="github logo" />
+	</a>
+	<a href="https://stromgedacht.de" target="_blank">StromGedacht</a>
+</footer>
