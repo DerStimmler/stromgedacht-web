05913d0c71a8b4f92ae0388ead4135894d67cfcf
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2024-01-17 21:51:21 +0100

    chore: update to svelte-kit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
